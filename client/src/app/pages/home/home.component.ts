d671c80314140c600442e76ea95c55629afc635c
谢陈东 <chendong.wings@gmail.com>
2019-02-23 17:08:22 +0800

    no message

--- client/src/app/pages/home/home.component.ts
+++ client/src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   list: any[] = [
-    { id: 11, name: 'Mr. Nice' },
+    { id: 11, title: 'Mr. Nice', image: 'https://cdn-images-1.medium.com/max/400/1*u4KmESpudYo7qP1Y8Pt1PQ.jpeg' },
     { id: 12, name: 'Narco' },
     { id: 13, name: 'Bombasto' },
     { id: 14, name: 'Celeritas' },
