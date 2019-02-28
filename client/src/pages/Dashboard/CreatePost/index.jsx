9db81a0cce676d17f599c2a36b3018556e6bb87b
chendong.xie <chendong.xie@innocellence.com>
2019-02-28 19:08:35 +0800

    feat: add create post page

---
+++ client/src/pages/Dashboard/CreatePost/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './index.scss';
+
+export default class CreatePost extends React.Component {
+  render() {
+    return (
+      <div className="create-post-container" />
+    );
+  }
+}
