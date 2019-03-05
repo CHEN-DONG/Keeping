7d022f1ebf4eb6cb3f7df41ee9a21f52341639f1
chendong.xie <chendong.xie@innocellence.com>
2019-03-05 15:13:36 +0800

    update

---
+++ client/src/layouts/DashboardLayout/Header/index.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Avatar, Row, Input, Menu, Icon } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import './index.scss';
+
+const Search = Input.Search;
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header-container">
+        <Row className="header" type="flex" justify="space-between" align="middle">
+          <Row className="left-content" type="flex" align="middle">
+            <div className="header-logo">
+              <Avatar shape="square" size={50} src={require('./assets/logo.jpeg')} />
+            </div>
+            <Menu
+              onClick={this.handleNavClick}
+              selectedKeys={[this.state.current]}
+              mode="horizontal"
+              style={{ lineHeight: '60px' }}
+            >
+              {
+                this.headerMenus.map((item, index) => {
+                  return (
+                    <Menu.Item key={index}>
+                      <Link to={item.path}>
+                        <Icon type={item.icon} />
+                        {item.name}
+                      </Link>
+                    </Menu.Item>
+                  );
+                })
+              }
+            </Menu>
+          </Row>
+          <div className="right-content">
+            <div className="header-search">
+              <Search
+                placeholder="搜索"
+                onSearch={this.handleSearch}
+                style={{ width: 200 }}
+              />
+            </div>
+          </div>
+        </Row>
+      </header>
+    );
+  }
+
+  handleNavClick = (e) => {
+    this.setState({
+      current: e.key,
+    });
+  }
+
+  handleSearch = (val) => {
+    this.props.history.push(`/search/${val}`);
+  }
+}
+
+export default withRouter(Header);
