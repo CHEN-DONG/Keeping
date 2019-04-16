f61e5f76dd4b6761cb9e0d4e87c87aab2efe04f9
CHEN-DONG <chendong.wings@gmail.com>
2019-04-16 23:56:35 +0800

    update

--- server/src/guards/local.guard.ts
+++ server/src/guards/local.guard.ts
@@ -3,9 +3,7 @@ import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
 @Injectable()
 export class LocalGuard implements CanActivate {
   public canActivate(context: ExecutionContext): boolean {
-
     const request = context.switchToHttp().getRequest();
-    console.log(request.user)
     return request.user;
   }
 }
